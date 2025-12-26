0f638134fab8b5b54a284f92738648e7219ed52c
Daniel Cunney <127962888+DanielCNY@users.noreply.github.com>
2025-12-27 00:36:46 +0100

    Set up camera movement
    Added simple shaders + coordinate axes + camera movement controls (w,a,s,d,space,tab)

--- scene/main.cpp
+++ scene/main.cpp
@@ -3,6 +3,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <./render/shader.h>
+
 #include <iostream>
 
 static GLFWwindow *window;
@@ -12,13 +14,109 @@ static int windowHeight = 768;
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
 
 // Camera
-static glm::vec3 eye_center(0.0f, 100.0f, 300.0f);
+static glm::vec3 eye_center(150.0f, 150.0f, 150.0f);
 static glm::vec3 lookat(0.0f, 0.0f, 0.0f);
 static glm::vec3 up(0.0f, 1.0f, 0.0f);
 
-static float FoV = 45.0f;
-static float zNear = 0.1f;
+static float FoV = 60.0f;
+static float zNear = 1.0f;
 static float zFar = 1000.0f;
+
+// Add this struct definition
+struct AxisXYZ {
+    // A structure for visualizing the global 3D coordinate system
+
+	GLfloat vertex_buffer_data[18] = {
+		// X axis
+		0.0, 0.0f, 0.0f,
+		100.0f, 0.0f, 0.0f,
+
+		// Y axis
+		0.0f, 0.0f, 0.0f,
+		0.0f, 100.0f, 0.0f,
+
+		// Z axis
+		0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 100.0f,
+	};
+
+	GLfloat color_buffer_data[18] = {
+		// X, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Y, green
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Z, blue
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+	};
+
+	// OpenGL buffers
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint colorBufferID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint programID;
+
+	void initialize() {
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../scene/shaders/simple.vert", "../scene/shaders/simple.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glm::mat4 mvp = cameraMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+        // Draw the lines
+        glDrawArrays(GL_LINES, 0, 6);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteProgram(programID);
+	}
+};
 
 int main(void)
 {
@@ -57,6 +155,9 @@ int main(void)
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
 
+	AxisXYZ axes;
+	axes.initialize();
+
     // Camera setup
     glm::mat4 projectionMatrix = glm::perspective(glm::radians(FoV), (float)windowWidth / windowHeight, zNear, zFar);
 
@@ -69,6 +170,8 @@ int main(void)
         glm::mat4 viewMatrix = glm::lookAt(eye_center, lookat, up);
         glm::mat4 vp = projectionMatrix * viewMatrix;
 
+    	axes.render(vp);
+
         // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -80,6 +183,50 @@ int main(void)
 
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+	float speed = 3.0f;
+	glm::vec3 lookDirection = glm::normalize(lookat - eye_center);
+	glm::vec3 rightOfDirection = glm::normalize(glm::cross(lookDirection, up));
+
+	// Move forward/backward: w/s
+	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = lookDirection * speed;
+		eye_center += move;
+		lookat += move;
+	}
+	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = lookDirection * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+
+	// Move left/right: a/d
+	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		
+		glm::vec3 move = rightOfDirection * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		
+		glm::vec3 move = rightOfDirection * speed;
+		eye_center += move;
+		lookat += move;
+	}
+
+	// Move up/down: space/tab
+	if (key == GLFW_KEY_SPACE && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = glm::normalize(up) * speed;
+		eye_center += move;
+		lookat += move;
+	}
+	if (key == GLFW_KEY_TAB && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = glm::normalize(up) * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+
+	// Close window
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
