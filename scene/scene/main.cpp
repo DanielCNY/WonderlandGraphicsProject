e3febc517a89f9737e711da0d295a6b84465c35f
Daniel Cunney <127962888+DanielCNY@users.noreply.github.com>
2025-12-28 16:25:37 +0100

    Added First-Person lookAt controls
    Implemented cursor callback to change the focus of the camera.

---
+++ scene/scene/main.cpp
@@ -0,0 +1,274 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <./render/shader.h>
+
+#include <iostream>
+
+static GLFWwindow *window;
+static int windowWidth = 1024;
+static int windowHeight = 768;
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+static void cursor_callback(GLFWwindow* window, double xpos, double ypos);
+
+// Camera
+static glm::vec3 eye_center(150.0f, 150.0f, 150.0f);
+static glm::vec3 lookat(0.0f, 0.0f, 0.0f);
+static glm::vec3 up(0.0f, 1.0f, 0.0f);
+
+static float FoV = 60.0f;
+static float zNear = 1.0f;
+static float zFar = 1000.0f;
+
+// Mouse movement
+static float yaw = -90.0f;
+static float pitch = 0.0f;
+static bool firstMouse = true;
+static float lastX = 512.0f;
+static float lastY = 384.0f;
+static float sensitivity = 0.1f;
+
+// Add this struct definition
+struct AxisXYZ {
+    // A structure for visualizing the global 3D coordinate system
+
+	GLfloat vertex_buffer_data[18] = {
+		// X axis
+		0.0, 0.0f, 0.0f,
+		100.0f, 0.0f, 0.0f,
+
+		// Y axis
+		0.0f, 0.0f, 0.0f,
+		0.0f, 100.0f, 0.0f,
+
+		// Z axis
+		0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 100.0f,
+	};
+
+	GLfloat color_buffer_data[18] = {
+		// X, red
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+
+		// Y, green
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+
+		// Z, blue
+		0.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f,
+	};
+
+	// OpenGL buffers
+	GLuint vertexArrayID;
+	GLuint vertexBufferID;
+	GLuint colorBufferID;
+
+	// Shader variable IDs
+	GLuint mvpMatrixID;
+	GLuint programID;
+
+	void initialize() {
+		// Create a vertex array object
+		glGenVertexArrays(1, &vertexArrayID);
+		glBindVertexArray(vertexArrayID);
+
+		// Create a vertex buffer object to store the vertex data
+		glGenBuffers(1, &vertexBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
+		// Create a vertex buffer object to store the color data
+		glGenBuffers(1, &colorBufferID);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(color_buffer_data), color_buffer_data, GL_STATIC_DRAW);
+
+		// Create and compile our GLSL program from the shaders
+		programID = LoadShadersFromFile("../scene/shaders/simple.vert", "../scene/shaders/simple.frag");
+		if (programID == 0)
+		{
+			std::cerr << "Failed to load shaders." << std::endl;
+		}
+
+		// Get a handle for our "MVP" uniform
+		mvpMatrixID = glGetUniformLocation(programID, "MVP");
+	}
+
+	void render(glm::mat4 cameraMatrix) {
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorBufferID);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glm::mat4 mvp = cameraMatrix;
+		glUniformMatrix4fv(mvpMatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+        // Draw the lines
+        glDrawArrays(GL_LINES, 0, 6);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+	}
+
+	void cleanup() {
+		glDeleteBuffers(1, &vertexBufferID);
+		glDeleteBuffers(1, &colorBufferID);
+		glDeleteVertexArrays(1, &vertexArrayID);
+		glDeleteProgram(programID);
+	}
+};
+
+int main(void)
+{
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(windowWidth, windowHeight, "Wonderland Project", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to open a GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, cursor_callback);
+
+    // Load OpenGL
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0)
+    {
+        std::cerr << "Failed to initialize OpenGL context." << std::endl;
+        return -1;
+    }
+
+    // Setup
+    glClearColor(0.1f, 0.1f, 0.12f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+	AxisXYZ axes;
+	axes.initialize();
+
+    // Camera setup
+    glm::mat4 projectionMatrix = glm::perspective(glm::radians(FoV), (float)windowWidth / windowHeight, zNear, zFar);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Camera/view matrix
+        glm::mat4 viewMatrix = glm::lookAt(eye_center, lookat, up);
+        glm::mat4 vp = projectionMatrix * viewMatrix;
+
+    	axes.render(vp);
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	float speed = 3.0f;
+	glm::vec3 lookDirection = glm::normalize(lookat - eye_center);
+	glm::vec3 rightOfDirection = glm::normalize(glm::cross(lookDirection, up));
+
+	// Move forward/backward: w/s
+	if (key == GLFW_KEY_W && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = lookDirection * speed;
+		eye_center += move;
+		lookat += move;
+	}
+	if (key == GLFW_KEY_S && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = lookDirection * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+
+	// Move left/right: a/d
+	if (key == GLFW_KEY_A && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		
+		glm::vec3 move = rightOfDirection * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+	if (key == GLFW_KEY_D && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		
+		glm::vec3 move = rightOfDirection * speed;
+		eye_center += move;
+		lookat += move;
+	}
+
+	// Move up/down: space/tab
+	if (key == GLFW_KEY_SPACE && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = glm::normalize(up) * speed;
+		eye_center += move;
+		lookat += move;
+	}
+	if (key == GLFW_KEY_TAB && (action == GLFW_REPEAT || action == GLFW_PRESS)) {
+		glm::vec3 move = glm::normalize(up) * speed;
+		eye_center -= move;
+		lookat -= move;
+	}
+
+	// Close window
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+static void cursor_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (firstMouse) {
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	// Offset like lab3 with sensitivity
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+	lastX = xpos;
+	lastY = ypos;
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// Constrain pitch
+	if (pitch > 89.0f) pitch = 89.0f;
+	if (pitch < -89.0f) pitch = -89.0f;
+
+	// Update lookat
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+	lookat = eye_center + glm::normalize(front);
+}
